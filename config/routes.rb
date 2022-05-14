1d783e8be6cd1b485c6734ad504ffa98120049e4
AlexIlichev <sapfir8500@mail.ru>
2022-05-14 16:55:21 +0400

    created method create for question

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get '/questions', to: 'questions#index'
 
   get '/questions/new', to: 'questions#new'
+
+  post '/questions', to: 'questions#create'
+
+  
   root 'pages#index'
 end
